bd4c98d5a08d80c615920d0e1dc51882685ebd4c
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-03-10 15:09:53 +0700

    Update readme and run script.

--- run.sh
+++ run.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# make sure compose is really, really down
+# Make sure compose is really, really down.
 docker compose down --remove-orphans
 docker compose down --remove-orphans
 
-# make sure no volumes exist
+# Make sure no volumes exist.
 docker volume rm --force letsencrypt-docker_certbot_conf
 docker volume rm --force letsencrypt-docker_certbot_www
 
-# start just the nginx service
-# copies in fake cert and key (example.com)
+# Start just the nginx service which
+# copies in fake cert and key (example.com).
 docker compose up --build -d nginx
 
-# check ngix should be running OK
+# Check ngix, should be running OK.
 docker compose ps
 
-# delete dir of fake certs, otherwise certbot will fail at next step
+# Delete dir of fake certs, otherwise certbot will fail at next step.
 docker compose exec nginx rm -rf /etc/letsencrypt/live/edition.christaylordeveloper.co.uk/
 
-# run up certbot container too, should get real certs
+# Run-up certbot container too, should now get real certs.
 docker compose run --build certbot
+
+# Restart nginx.
+docker compose restart nginx
+
+# Check if domain has TLS
+curl https://edition.christaylordeveloper.co.uk
