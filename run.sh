cbb76f6437c18b04bb539ff5d49d5e03a55bbdf3
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-03-10 11:30:19 +0700

    Compose cmd to remove dir.

--- run.sh
+++ run.sh
@@ -7,4 +7,5 @@ docker volume rm --force letsencrypt-docker_certbot_www
 docker compose up --build -d nginx
 # ngix should be running OK
 docker compose ps
+docker compose exec nginx rm -rf /etc/letsencrypt/live/editions.christaylordeveloper.co.uk/
 docker compose run --build certbot
