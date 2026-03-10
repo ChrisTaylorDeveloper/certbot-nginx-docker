9a298e925e56e3d0ee87dc1522dad1fc84efc884
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-03-10 10:58:08 +0700

    Changes.

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 docker compose down --remove-orphans
 docker compose down --remove-orphans
-git pull
 docker volume rm --force letsencrypt-docker_certbot_conf
 docker volume rm --force letsencrypt-docker_certbot_www
 docker compose up --build -d nginx
+# ngix should be running OK
 docker compose ps
 docker compose run --build certbot
