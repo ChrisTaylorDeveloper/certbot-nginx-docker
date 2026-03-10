706342b689d3699c10caebf59678e6c33c3dc232
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-03-10 10:53:42 +0700

    Add run script.

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker compose down --remove-orphans
+docker compose down --remove-orphans
+git pull
+docker volume rm --force letsencrypt-docker_certbot_conf
+docker volume rm --force letsencrypt-docker_certbot_www
+docker compose up --build -d nginx
