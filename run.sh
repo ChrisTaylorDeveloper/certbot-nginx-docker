85ac18cc99b799ebb367e1591e1e5badfac2c9cd
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-03-10 08:34:49 +0700

    Update script.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker compose down --remove-orphans
+docker compose down --remove-orphans
 docker volume rm $(docker volume ls -q)
 docker compose build --no-cache
 docker compose up
