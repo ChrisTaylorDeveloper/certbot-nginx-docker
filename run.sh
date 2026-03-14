a3c46a6ebcd1da9c34d7a3329ec7225aa27d6b70
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-03-14 12:06:52 +0700

    Change script.

--- run.sh
+++ run.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-# Start with no services.
+# Clean up from previous run.
 docker compose down --remove-orphans
-
-# Make sure no volumes exist.
 docker volume rm --force certbot_conf
 docker volume rm --force certbot_www
 
-# Build nginx service with minimal
-# conf required to get the certs.
+# Build and run up nginx service, with
+# minimal conf required to get the certs.
 docker compose build --build-arg NGINX_CONF=certs-issue nginx
-
-# Start nginx service.
 docker compose up -d nginx
 
-# Now ask certbot to get certs. Remove --dry-run !
-docker compose run --build --rm certbot certonly --dry-run --webroot -w /var/www/certbot --force-renewal --email chris@christaylordeveloper.co.uk -d edition.christaylordeveloper.co.uk --agree-tos --no-eff-email
+# Now ask certbot to get certs.
+docker compose run --build --rm certbot certonly --webroot -w /var/www/certbot --force-renewal --email chris@christaylordeveloper.co.uk -d edition.christaylordeveloper.co.uk --agree-tos --no-eff-email
 
-# Build nginx service again but this
-# time with the production conf.
+# Build and run nginx service again but
+# this time with the production conf file.
 docker compose build --build-arg NGINX_CONF=production nginx
-
-# Start nginx.
 docker compose up -d nginx
 
 # Check if domain has TLS
