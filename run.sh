6eff4159b02c2a6957ae397564e7e8c08d4846c3
Chris Taylor <chris@christaylordeveloper.co.uk>
2026-03-14 11:48:32 +0700

    Change script.

--- run.sh
+++ run.sh
@@ -11,14 +11,11 @@ docker volume rm --force certbot_www
 # conf required to get the certs.
 docker compose build --build-arg NGINX_CONF=certs-issue nginx
 
+# Start nginx service first.
 docker compose up -d nginx
 
-# Check ngix, is running OK.
-docker compose ps
-
-# Now add in certbot. Should try a dry run certs issue.
-docker compose run --build --rm certbot \
-  certonly --dry-run --webroot -w /var/www/certbot --force-renewal --email chris@christaylordeveloper.co.uk -d edition.christaylordeveloper.co.uk --agree-tos --no-eff-email
+# Now ask certbot to get certs. Could also try --dry-run
+docker compose run --build --rm certbot certonly --webroot -w /var/www/certbot --force-renewal --email chris@christaylordeveloper.co.uk -d edition.christaylordeveloper.co.uk --agree-tos --no-eff-email
 
 # Restart nginx.
 # docker compose restart nginx
